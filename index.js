91734fc92bc341372ae563ec878df9685218a9e7
Jinhyun Kim <jinkim608@gmail.com>
2014-10-19 11:48:19 -0400

    6 test

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ app.get('/test', function(request, response) {
   response.send(result);
 });
 
-app.get('/db', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELCT * FROM test_table', function(err, result) {
-			done();
-			iff (err) {
-				console.error(err);
-				response.send("Error " + error);
-			} else {
-				response.send(result.rows);
-			}
-		});
-	});
-});
+// app.get('/db', function(request, response) {
+// 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+// 		client.query('SELCT * FROM test_table', function(err, result) {
+// 			done();
+// 			iff (err) {
+// 				console.error(err);
+// 				response.send("Error " + error);
+// 			} else {
+// 				response.send(result.rows);
+// 			}
+// 		});
+// 	});
+// });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
