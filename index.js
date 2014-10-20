3dff91306d7612071000e98a3eb17014c961f133
Jinhyun Kim <jinkim608@gmail.com>
2014-10-20 10:26:09 -0400

    POST /poll fixed

--- index.js
+++ index.js
@@ -2,90 +2,95 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var bodyParser = require('body-parser');
+
 // parse urlencoded request bodies into req.body
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({	extended: true }));
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
 
-/* ######## REST API ######## */
+/* -------- REST API -------- */
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+/* ######## DEBUG ######## */
+
+app.get('/api', function(req, res) {
+	res.send('Hello World!')
 });
 
-app.get('/poll', function(request, response) {
+app.get('/api/test', function(req, res) {
+	res.send("TEST 202");
+});
+
+app.get('/api/db', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM poll;', function(err, result) {
+		var queryString = "SELECT * FROM test_table";
+
+		client.query(queryString, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
-				response.send("Error " + err);
+				res.send("Error " + err);
 			} else {
-				response.send(result.rows);
+				res.send(result.rows);
 			}
 		});
 	});
 });
 
-app.get('/poll/:id', function(request, response) {
+/* ######## POLL ######## */
+
+app.get('/api/poll', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM poll WHERE poll_id='+request.params.id+';', function(err, result) {
+		var queryString = "SELECT * FROM poll";
+		
+		client.query(queryString, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
-				response.send("Error " + err);
+				res.send("Error " + err);
 			} else {
-				response.send(result.rows);
+				res.send(result.rows);
 			}
 		});
 	});
 });
 
-app.post('/poll', function(req, response) {
-
-	// response.send("HERE-- " + req.body);
-	// console.log(req.body);
-
+app.get('/api/poll/:id', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var q = "INSERT INTO poll (creator_id, question) VALUES ('"
+		var queryString = "SELECT * FROM poll WHERE poll_id=" + req.params.id + ";";
+		
+		client.query(queryString, function(err, result) {
+			done();
+			if (err) {
+				console.error(err);
+				res.send("Error " + err);
+			} else {
+				res.send(result.rows);
+			}
+		});
+	});
+});
+
+app.post('/api/poll', function(req, res) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		var queryString = "INSERT INTO poll (creator_id, question) VALUES ('"
 		+ req.body.creator_id + "', '" + req.body.question + "');";
 
-		client.query(q, function(err, result) {
+		client.query(queryString, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
-				response.send("Error " + err);
+				res.send("Error " + err);
 			} else {
-				response.send(req.headers + "<br>-------<br>" + result.rows);
+				res.send(result.rows);
 			}
 		});
 	});
 });
 
-app.get('/test', function(request, response) {
-	response.send("TEST 202");
-});
-
-app.get('/db', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
-			done();
-			if (err) {
-				console.error(err);
-				response.send("Error " + err);
-			} else {
-				response.send(result.rows);
-			}
-		});
-	});
-});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+	console.log("Node app is running at localhost:" + app.get('port'))
 });
