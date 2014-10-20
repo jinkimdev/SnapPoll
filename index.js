6a1d4add258de197b08b8417876ed89df57f2b2f
Jinhyun Kim <jinkim608@gmail.com>
2014-10-20 02:07:04 -0400

    POST /poll debug 10 fix

--- index.js
+++ index.js
@@ -50,20 +50,20 @@ app.get('/poll/:id', function(request, response) {
 
 app.post('/poll', function(req, response) {
 
-	response.send("HERE-- " + req.body);
+	// response.send("HERE-- " + req.body);
 
-	// pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 
-	// 	client.query("INSERT INTO poll (creator_id, question) VALUES ('" + req.body.creator_id + "', '" + req.body.question + "'');", function(err, result) {
-	// 		done();
-	// 		if (err) {
-	// 			console.error(err);
-	// 			response.send("Error " + err);
-	// 		} else {
-	// 			response.send(result.rows);
-	// 		}
-	// 	});
-	// });
+		client.query("INSERT INTO poll (creator_id, question) VALUES ('" + req.body.creator_id + "', '" + req.body.question + "'');", function(err, result) {
+			done();
+			if (err) {
+				console.error(err);
+				response.send("Error " + err);
+			} else {
+				response.send(result.rows);
+			}
+		});
+	});
 });
 
 app.get('/test', function(request, response) {
