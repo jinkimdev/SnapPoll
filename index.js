60371100873d16f2ed14c9cc363ac45b91595055
Jinhyun Kim <jinkim608@gmail.com>
2014-11-15 18:37:04 -0500

    Test on pg connect

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var bodyParser = require('body-parser');
+var router = express.Router();
+
+var dbcred = "postgres://riznmjlobcgmmm:PqcDsJ6S52QHqmwl_vemRPdd2K@ec2-54-204-32-91.compute-1.amazonaws.com:5432/d25fpp8hn5frvv";
 
 // parse urlencoded request bodies into req.body
 app.use(bodyParser.urlencoded({	extended: true }));
@@ -10,20 +13,24 @@ app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-
+// router setup
+router.get('/', function(req, res, next) {
+	next();
+})
 /* -------- REST API -------- */
 
 /* ######## DEBUG ######## */
 
-app.get('/api', function(req, res) {
+router.get('/debug', function(req, res) {
+	console.log(process.env.DATABASE_URL);
 	res.send('Hello World!')
 });
 
-app.get('/api/test', function(req, res) {
-	res.send("TEST 202");
+router.get('/test', function(req, res) {
+	res.send("TEST 33");
 });
 
-app.get('/api/db', function(req, res) {
+router.get('/db', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		var queryString = "SELECT * FROM test_table";
 
@@ -41,9 +48,12 @@ app.get('/api/db', function(req, res) {
 
 /* ######## POLL ######## */
 
-app.get('/api/poll', function(req, res) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString = "SELECT * FROM poll";
+router.get('/poll', function(req, res) {
+	console.log("#### dbcred #### " + dbcred);
+	pg.connect(dbcred, function(err, client, done) {
+
+		console.log("#### pg.connect #### " + client);
+		var queryString = "SELECT * FROM polls";
 		
 		client.query(queryString, function(err, result) {
 			done();
@@ -57,9 +67,9 @@ app.get('/api/poll', function(req, res) {
 	});
 });
 
-app.get('/api/poll/:id', function(req, res) {
+router.get('/poll/:id', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString = "SELECT * FROM poll WHERE poll_id=" + req.params.id + ";";
+		var queryString = "SELECT * FROM polls WHERE poll_id=" + req.params.id + ";";
 		
 		client.query(queryString, function(err, result) {
 			done();
@@ -73,7 +83,7 @@ app.get('/api/poll/:id', function(req, res) {
 	});
 });
 
-app.post('/api/poll', function(req, res) {
+router.post('/poll', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		var queryString = "INSERT INTO poll (creator_id, question) VALUES ('"
 		+ req.body.creator_id + "', '" + req.body.question + "');";
@@ -90,7 +100,7 @@ app.post('/api/poll', function(req, res) {
 	});
 });
 
-
+app.use('/api', router);
 app.listen(app.get('port'), function() {
 	console.log("Node app is running at localhost:" + app.get('port'))
 });
