7306828a663a1fc09879b30317928e5edb253de7
Jinhyun Kim <jinkim608@gmail.com>
2014-11-16 16:06:05 -0500

    Updated db queries with squel query builder

--- index.js
+++ index.js
@@ -17,50 +17,41 @@ router.get('/', function(req, res, next) {
 	next();
 })
 
-/* -------- REST API -------- */
+// when testing with local node server and local postgres db,
+// enter before running node: 
+// $ export DATABASE_URL=postgres:///$(whoami)
+var DEBUG = 1;
+
+/* ################ REST API ################ */
 
 /* -------- DEBUG -------- */
 
 router.get('/', function(req, res) {
-	console.log(process.env.DATABASE_URL);
-	res.send('Snap Poll REST API')
+	res.send('Snap Poll REST API');
+	if (DEBUG) {
+		console.log("DEBUG MODE");
+	}
 });
 
 router.get('/debug', function(req, res) {
-	console.log(process.env.DATABASE_URL);
-	res.send('Hello World!')
+	if (DEBUG) console.log(process.env.DATABASE_URL);
+	res.send('Hello World!');
 });
 
 router.get('/test', function(req, res) {
 	res.send("TEST 33");
 });
 
-router.get('/db', function(req, res) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString = "SELECT * FROM test_table";
-
-		client.query(queryString, function(err, result) {
-			done();
-			if (err) {
-				console.error(err);
-				res.send("Error " + err);
-			} else {
-				res.send(result.rows);
-			}
-		});
-	});
-});
 
 /* -------- POLL -------- */
 
 // GET - retrieve all polls in the table
 router.get('/poll', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		// console.log("#### pg.connect #### " + client);
-		// var queryString = "SELECT * FROM polls";
-		var queryString = squel.select().from('polls').toString();
-		
-		client.query(queryString, function(err, result) {
+		var q = squel.select()
+			.from('polls')
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -75,9 +66,10 @@ router.get('/poll', function(req, res) {
 // GET - retrieve a poll with the passed in poll_id
 router.get('/poll/:poll_id', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString = "SELECT * FROM polls WHERE poll_id=" + req.params.poll_id + ";";
-		
-		client.query(queryString, function(err, result) {
+		var q = squel.select().from('polls')
+			.where("poll_id='" + req.params.poll_id + "'")
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -92,13 +84,12 @@ router.get('/poll/:poll_id', function(req, res) {
 // POST - insert a poll created by a user (poll creator)
 router.post('/poll', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "INSERT INTO polls (creator_id, q_text, allow_multiple_responses) VALUES ('"
-				+ req.body.creator_id + "', '" 
-				+ req.body.question + "', '"
-				+ req.body.allow_multiple_responses + "');";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.insert().into('polls')
+			.set('creator_id', req.body.creator_id)
+			.set('q_text', req.body.q_text)
+			.set('allow_multiple_responses', req.body.allow_multiple_responses)
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -113,11 +104,10 @@ router.post('/poll', function(req, res) {
 // DELETE - delete a poll specified the poll_id
 router.delete('/poll', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "DELETE FROM polls WHERE poll_id='"
-				+ req.body.poll_id + "';";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.delete().from('polls')
+			.where('poll_id=' + req.body.poll_id)
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -134,13 +124,12 @@ router.delete('/poll', function(req, res) {
 // create a user, user_id should be unique
 router.post('/user', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "INSERT INTO users (user_id, f_name, l_name) VALUES ('"
-				+ req.body.user_id + "', '" 
-				+ req.body.f_name + "', '"
-				+ req.body.l_name + "');";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.insert().into('users')
+			.set('user_id', req.body.user_id)
+			.set('f_name', req.body.f_name)
+			.set('l_name', req.body.l_name)
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -155,12 +144,11 @@ router.post('/user', function(req, res) {
 // PUT - update user info
 router.put('/user', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "UPDATE users SET f_name='" + req.body.f_name
-				+ "', l_name='" + req.body.l_name + "'" 
-				+ " WHERE user_id='" + req.body.user_id + "';";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.update().table('users')
+			.set('f_name', req.body.f_name)
+			.set('l_name', req.body.l_name)
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -177,11 +165,10 @@ router.put('/user', function(req, res) {
 // GET - retrieve a response specified by response_id
 router.get('/response/:response_id', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "SELECT * FROM responses WHERE response_id='"
-				+ req.body.response_id + "';";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.select().from('responses')
+			.where("response_id='" + req.params.response_id + "'")
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -196,16 +183,14 @@ router.get('/response/:response_id', function(req, res) {
 // POST - insert a response to a poll
 router.post('/response', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "INSERT INTO responses (x, y, respondent_id, attribute_choice, poll_id)"
-				+ " VALUES ('"
-				+ req.body.x + "', '"
-				+ req.body.y + "', '"
-				+ req.body.respondent_id + "', '"
-				+ req.body.attribute_choice + "', '"
-				+ req.body.poll_id + "';";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.insert().into('responses')
+			.set('x', req.body.x)
+			.set('y', req.body.y)
+			.set('respondent_id', req.body.respondent_id)
+			.set('attribute_choice', req.body.attribute_choice)
+			.set('poll_id', req.body.poll_id)
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -220,14 +205,14 @@ router.post('/response', function(req, res) {
 // PUT - update a response specified by response_id
 router.put('/response', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "UPDATE responses SET x='" + req.body.x + "', "
-				+ "y='" + req.body.y + "', "
-				+ "respondent_id='" + req.body.respondent_id + "', "
-				+ "attribute_choice='" + req.body.attribute_choice + "' "
-				+ "WHERE response_id='" + req.body.response_id + "';";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.update().table('respondes')
+			.set('x', req.body.x)
+			.set('y', req.body.y)
+			.set('respondent_id', req.body.respondent_id)
+			.set('attribute_choice', req.body.attribute_choice)
+			.set('poll_id', req.body.poll_id)
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
@@ -244,13 +229,12 @@ router.put('/response', function(req, res) {
 /* -------- RESULT -------- */
 
 // GET - retrieve all responses to the specified poll_id
-router.get('/result', function(req, res) {
+router.get('/result/:poll_id', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		var queryString
-			= "SELECT * FROM responses WHERE poll_id='"
-				+ req.body.poll_id + "';";
-
-		client.query(queryString, function(err, result) {
+		var q = squel.select().from('responses')
+			.where("poll_id=" + req.params.poll_id)
+			.toString();
+		client.query(q, function(err, result) {
 			done();
 			if (err) {
 				console.error(err);
